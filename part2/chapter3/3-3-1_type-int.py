cb4f711df146af4c28698b2f931a9822de35856c
babas <shum_baba0206@yahoo.co.jp>
2023-06-22 21:11:52 +0900

    3章 3-3-1(数値型)

---
+++ part2/chapter3/3-3-1_type-int.py
@@ -0,0 +1,45 @@
+posnum = +12
+negnum = -24
+
+print(posnum)
+print(negnum)
+
+negnum = -posnum
+
+print(negnum)
+print(-negnum)
+
+
+print(0x11)
+print(0xff)
+print(0xffff)
+
+print(0b01010)
+print(0o12)
+
+
+print(bin(10))
+print(oct(10))
+print(hex(10))
+
+
+print(int())
+print(int('5'))
+
+
+# print(int('a'))
+# Traceback (most recent call last):
+#   File "/Users/babaShumpei/Desktop/work/shbaba/perfect-python/part2/chapter3/3-3-1_type-int.py", line 29, in <module>
+#     print(int('a'))
+#           ^^^^^^^^
+# ValueError: invalid literal for int() with base 10: 'a'
+
+print(int('a', 16))
+print(int('z', 36))
+print(int('py', 36))
+
+
+print(3/2)
+print(3//2)
+
+
